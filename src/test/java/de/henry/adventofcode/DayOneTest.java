b6a2e00be40c349b19125e5e625a4c7ff4f7e4d6
henry <henry@dasburo.com>
2020-12-03 16:27:02 +0100

    DayOne

---
+++ src/test/java/de/henry/adventofcode/DayOneTest.java
@@ -0,0 +1,19 @@
+package de.henry.adventofcode;
+
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class DayOneTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testDayOne() {
+        String[] args = {};
+        DayOne.main(args);
+    }
+}
