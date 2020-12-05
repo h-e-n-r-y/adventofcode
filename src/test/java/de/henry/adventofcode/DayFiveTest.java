1d39cb3da008ac2032dcd108ac00471575117930
henry <henry@dasburo.com>
2020-12-05 17:54:29 +0100

    Day 5

---
+++ src/test/java/de/henry/adventofcode/DayFiveTest.java
@@ -0,0 +1,16 @@
+package de.henry.adventofcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DayFiveTest {
+
+	@Test
+	void testPassId() {
+		assertEquals(567, DayFive.passId("BFFFBBFRRR"));
+		assertEquals(119, DayFive.passId("FFFBBBFRRR"));
+		assertEquals(820, DayFive.passId("BBFFBBFRLL"));
+	}
+
+}
