7c06c3d460c29666b0f79a5fed4770f657adab8e
henry <henry@dasburo.com>
2020-12-08 19:22:04 +0100

    Day Six Part 1

---
+++ src/test/java/de/henry/adventofcode/DaySixTest.java
@@ -0,0 +1,27 @@
+package de.henry.adventofcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DaySixTest {
+
+	@Test
+	void testCount() {
+		assertEquals(3, DaySix.count("abc"));
+		assertEquals(3, DaySix.count("a\nb\nc"));
+		assertEquals(3, DaySix.count("ab\nac"));
+		assertEquals(1, DaySix.count("a\na\na\ba"));
+		assertEquals(1, DaySix.count("b"));
+	}
+
+	@Test
+	void testCount2() {
+		assertEquals(3, DaySix.count2("abc"));
+		assertEquals(3, DaySix.count2("a\nb\nc"));
+		assertEquals(1, DaySix.count2("ab\nac"));
+		assertEquals(1, DaySix.count2("a\na\na\ba"));
+		assertEquals(1, DaySix.count2("b"));
+	}
+
+}
